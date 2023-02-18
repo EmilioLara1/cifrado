df04d54c049a6ac2941020f984190e846eea4d0b
EmilioLara1 <emiliolarap@gmail.com>
2023-02-18 11:39:12 -0600

    Polybios

--- cifrados.py
+++ cifrados.py
@@ -4,12 +4,55 @@ import os
 # Se crea una constate de el abecedario 
 abecedario=("abcdefghijklmnñopqrstuvwxyz")
 
+
 # Cesar funciones
 
 
 # Polybios funciones
+def poCifra(mensaje):
+    cifrado = ""
 
+    for char in mensaje:
+        #Mapea la matriz en 5x5 iniciando por fila y luego por columna con el alfabeto
+        row = int((ord(char) - ord("a")) / 5) + 1
+        col = ((ord(char) - ord("a")) % 5) + 1
 
+        #Se recorren las filas
+        if char == "k":
+            row = row - 1
+            col = 5 - col + 1
+
+        # Se arregla un bug y junta en el mismo campo j y i
+        elif ord(char) >= ord("j"):
+            if col == 1:
+                col = 6
+                row = row -1
+            col = col -1
+        
+        #Se asignan los valores y se convierten en string
+        r = str(row)
+        c = str(col)
+        cifrado = cifrado + r + c
+    return cifrado
+
+def poDecifrar(mensaje):
+    #Se crea una lista que contiene el mensaje
+    mensaje1 = list(mensaje)
+    decifrado = ""
+
+    #Se divide la lista en pares para poder sacar el codigo de las letras
+    for i in range (0, len(mensaje), 2):
+        # Se asigna el valor de la fila y de la columna
+        r = int(mensaje1[i])
+        c = int(mensaje1[i+1])
+
+        # Se agrega el 96 debido a que 
+        ch = chr(((r-1) * 5 + c + 96))
+        if (ord(ch)-96 >= 10):
+            ch = chr(((r-1) * 5 + c + 96 + 1))
+        ch1 = str(ch)
+        decifrado = decifrado + ch1
+    return decifrado
 
 # Vigenere funciones
 def vicifrar(mensaje, key):
@@ -48,10 +91,31 @@ def main():
 
 
         # Opciones de Polybios
+        if (opt1==2):
+            print ("|--------------|")
+            print ("|  Polybios    |")
+            print ("|  1 Cifrar    |")
+            print ("|  2 Descifrar |")
+            print ("|--------------|")
+            opt2=int(input("Ingrese la opción: "))
+
+            if (opt2==1):
+                #Cifrado sin espacios porque le da amsieda
+                c = input("Ingrese el mensaje a cifrar sin espacios:")
+                c = c.lower()
+                cifra = poCifra(c)
+                print("El mensaje cifrado es: ",cifra)
+            if (opt2==2):
+                #Decifrado
+                c = input("Ingrese el mensaje a decifrar: ")
+                decifrado = poDecifrar(c)
+                print("El mensaje decifrado es: ",decifrado)
+
 
         #Opciones de Vigenere
         if (opt1==3):
             print ("|--------------|")
+            print ("|  Vigenere    |")
             print ("|  1 Cifrar    |")
             print ("|  2 Descifrar |")
             print ("|--------------|")
